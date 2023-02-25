bb054f0766396303fccc298d325498f644f28d82
Electronics Tinkerer <electronicstinkerer@gmail.com>
2023-02-25 16:02:19 -0500

    Refactor entire message print system

---
+++ src/message.c
@@ -0,0 +1,70 @@
+/**
+ * SLIME assembler
+ * An improved HEIPA assembler replacement written in C
+ * 
+ * Message printer
+ * 
+ * (C) Ray Clemens 2023
+ */
+
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "message.h"
+
+/**
+ * Print a message
+ * 
+ * @param type The prefix of the message
+ * @param linenum The line number
+ * @param fmt* The vprintf format string
+ * @param ... The args to the format string
+ * @return The number of characters written
+ */
+int pmsg(msg_kind_t type, ssize_t linenum, const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    
+    int len = 0;
+    switch (type) {
+    case M_ERRR:
+        len += fprintf(stderr, "\x1b[1;31;40m[ERRR]\x1b[0m ");
+        if (linenum >= 0) {
+            len += fprintf(stderr, "line %ld: ", linenum);
+        }
+        len += vfprintf(stderr, fmt, args);
+        break;
+    case M_WARN:
+        len += fprintf(stdout, "\x1b[1;33;40m[WARN]\x1b[0m ");
+        if (linenum >= 0) {
+            len += fprintf(stdout, "line %ld: ", linenum);
+        }
+        len += vfprintf(stdout, fmt, args);
+        break;
+    case M_INFO:
+        len += fprintf(stdout, "\x1b[1;36;40m[INFO]\x1b[0m ");
+        if (linenum >= 0) {
+            len += fprintf(stdout, "line %ld: ", linenum);
+        }
+        len += vfprintf(stdout, fmt, args);
+        break;
+    case M_DBUG:
+        len += fprintf(stdout, "\x1b[1;35;40m[DBUG]\x1b[0m ");
+        if (linenum >= 0) {
+            len += fprintf(stdout, "line %ld: ", linenum);
+        }
+        len += vfprintf(stdout, fmt, args);
+        break;
+    default:
+        len += fprintf(stdout, "\x1b[1;32;40m[????]\x1b[0m ");
+        if (linenum >= 0) {
+            len += fprintf(stdout, "line %ld: ", linenum);
+        }
+        len += vfprintf(stdout, fmt, args);
+        break;
+    }
+    va_end(args);
+    return len;
+}
+
---
+++ src/message.h
@@ -0,0 +1,24 @@
+/**
+ * SLIME assembler
+ * An improved HEIPA assembler replacement written in C
+ * 
+ * Message printer
+ * 
+ * (C) Ray Clemens 2023
+ */
+
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+typedef enum msg_kind_t {
+    M_ERRR = 0,
+    M_WARN,
+    M_INFO,
+    M_DBUG
+} msg_kind_t;
+
+int pmsg(msg_kind_t, ssize_t, const char *, ...);
+
+#endif
+
+
