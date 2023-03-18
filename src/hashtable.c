ca1b62e2d361d47ff43095eaae733a8c7bea603e
Electronics Tinkerer <electronicstinkerer@gmail.com>
2023-03-18 16:48:07 -0400

    Refactor HT for stricter type checking

--- src/hashtable.c
+++ src/hashtable.c
@@ -8,6 +8,8 @@
  * 2023-02-09: Changed to macro cast wrappers
  * 2023-02-10: Fixed "definitely" and "indirectly" lost
  *             memory on table resize (valgrind)
+ * 2023-03-18: Refactor to add type checking for all 
+ *             functions
  */
 
 #define __HT_HT_C
@@ -15,7 +17,7 @@
 #undef __HT_HT_C
 
 // Helper functions
-static inline unsigned int _ht_compute_index(ht_t *table, unsigned long key);
+static inline ht_index_t _ht_compute_index(ht_t *table, ht_key_t key);
 // static inline float _ht_get_collision_average(ht_t *table);
 static inline float _ht_get_load_factor(ht_t *table);
 static void _ht_double_table(ht_t *table);
@@ -25,16 +27,16 @@ static void _ht_half_table(ht_t *table);
 /**
  * Initialize a hashtable struct
  * 
- * @return 0 on success
- *         1 on failure (memory allocation failure)
+ * @return false on success
+ *         true on failure (memory allocation failure)
  *         
  */
-int ht_init(ht_t **table)
+bool __ht_init(ht_t **table)
 {
     *table = malloc(sizeof(**table));
     
     if (!*table) {
-        return 1; // Unable to malloc memory
+        return true; // Unable to malloc memory
     }
     
     (*table)->arraySize = HT_INITIAL_SIZE;
@@ -44,13 +46,13 @@ int ht_init(ht_t **table)
     (*table)->table = malloc(sizeof(*((*table)->table)) * (*table)->arraySize);
 
     if ((*table)->table == NULL) {
-        return 1;    // Unable to malloc memory
+        return true;    // Unable to malloc memory
     }
     
     for (int i = 0; i < (*table)->arraySize; i++) {
         (*table)->table[i] = NULL;
     }
-    return 0;
+    return false;
 }
 
 
@@ -60,7 +62,7 @@ int ht_init(ht_t **table)
  * 
  * @param table The hashtable to be cleared
  */
-void ht_clear(ht_t *table)
+void __ht_clear(ht_t *table)
 {
     if (table != NULL)
     {
@@ -89,11 +91,11 @@ void ht_clear(ht_t *table)
  * @param key The key associated with the provided value
  * @param value The value to be associated with the key
  */
-void __ht_put(ht_t *table, unsigned long key, void *value)
+void __ht_put(ht_t *table, ht_key_t key, void *value)
 {
     if (table != NULL)
     {
-        unsigned int pointer = _ht_compute_index(table, key);
+        ht_index_t pointer = _ht_compute_index(table, key);
 
         ht_entry_t *item = malloc(sizeof(*item));
         
@@ -166,7 +168,7 @@ void __ht_sput(ht_t *table, char *key, void *value)
 {
     if (table != NULL && key != NULL)
     {
-        unsigned long keyi = ht_hash_string(key);
+        ht_key_t keyi = __ht_hash_string(key);
         __ht_put(table, keyi, value);
     }
 }
@@ -180,12 +182,12 @@ void __ht_sput(ht_t *table, char *key, void *value)
  * @return The value corresponding to the key specified 
  *         (NULL if table is NULL or it element does not exist)
  */
-void *__ht_get(ht_t *table, unsigned long key)
+void *__ht_get(ht_t *table, ht_key_t key)
 {
     if (table != NULL)
     {
         // Calculate the location in the table
-        unsigned int hashValue = _ht_compute_index(table, key);
+        ht_index_t hashValue = _ht_compute_index(table, key);
         ht_entry_t *node = table->table[hashValue];
 
         while (node != NULL)
@@ -214,7 +216,7 @@ void *__ht_sget(ht_t *table, char *key)
     if (table != NULL && key != NULL)
     {
         // Calculate the location in the table
-        unsigned long keyi = ht_hash_string(key);
+        ht_key_t keyi = __ht_hash_string(key);
         return __ht_get(table, keyi);
     }
     return NULL;
@@ -228,12 +230,12 @@ void *__ht_sget(ht_t *table, char *key)
  * @param key The key for the value to be removed
  * @return The value associated with the key, NULL if no such element exists
  */
-void *__ht_remove(ht_t *table, unsigned long key)
+void *__ht_remove(ht_t *table, ht_key_t key)
 {
     if (table != NULL)
     {
         // Calculate location in the table
-        unsigned int hashValue = _ht_compute_index(table, key);
+        ht_index_t hashValue = _ht_compute_index(table, key);
         ht_entry_t *node = table->table[hashValue];
 
         ht_entry_t *previousNode = NULL;
@@ -291,7 +293,7 @@ void *__ht_sremove(ht_t *table, char *key)
     if (table != NULL)
     {
         // Calculate location in the table
-        unsigned long keyi = ht_hash_string(key);
+        ht_key_t keyi = __ht_hash_string(key);
         return __ht_remove(table, keyi);
     }
     return NULL; // Not Found
@@ -303,27 +305,27 @@ void *__ht_sremove(ht_t *table, char *key)
  * 
  * @param table The table in which to check for the key
  * @param key The key to search for
- * @return 1 if the key is found, 
- *         0 if not found or if table is NULL
+ * @return true if the key is found, 
+ *         false if not found or if table is NULL
  */
-unsigned int ht_contains_key(ht_t *table, unsigned long key)
+bool __ht_contains_key(ht_t *table, ht_key_t key)
 {
     if (table != NULL)
     {
         // Calculate location in the ht
-        unsigned int hashValue = _ht_compute_index(table, key);
+        ht_index_t hashValue = _ht_compute_index(table, key);
         ht_entry_t *node = table->table[hashValue];
 
         while (node != NULL)
         {
             if (node->key == key)
             {
-                return 1;
+                return true;
             }
             node = node->next;
         }
     }
-    return 0;
+    return false;
 }
 
 
@@ -335,25 +337,25 @@ unsigned int ht_contains_key(ht_t *table, unsigned long key)
  * @return 1 if the key is found, 
  *         0 if not found or if table is NULL
  */
-unsigned int ht_contains_skey(ht_t *table, char *key)
+bool __ht_contains_skey(ht_t *table, char *key)
 {
     if (table != NULL)
     {
         // Calculate location in the ht
-        unsigned long keyi = ht_hash_string(key);
-        unsigned int hashValue = _ht_compute_index(table, keyi);
+        ht_key_t keyi = __ht_hash_string(key);
+        ht_index_t hashValue = _ht_compute_index(table, keyi);
         ht_entry_t *node = table->table[hashValue];
 
         while (node != NULL)
         {
             if (node->key == keyi)
             {
-                return 1;
+                return true;
             }
             node = node->next;
         }
     }
-    return 0;
+    return false;
 }
 
 
@@ -364,12 +366,12 @@ unsigned int ht_contains_skey(ht_t *table, char *key)
  * 
  * @param table The table to free
  */
-void ht_destroy(ht_t **table)
+void __ht_destroy(ht_t **table)
 {
     if (table != NULL && *table != NULL)
     {
         ht_entry_t *node, *node2;
-        for (unsigned int i = 0; i < (*table)->arraySize; i++)
+        for (ht_index_t i = 0; i < (*table)->arraySize; i++)
         {
             if ((*table)->table[i] != NULL)
             {
@@ -394,14 +396,14 @@ void ht_destroy(ht_t **table)
  * Returns if the hashtable contains 0 elements
  *
  * @param table The table to check if empty
- * @return True (1) if there are 0 elements in the HashTable, 
- *         False (0) if not empty or if NULL table
+ * @return True if there are 0 elements in the HashTable, 
+ *         False if not empty or if NULL table
  */
-unsigned int ht_is_empty(ht_t *table) 
+bool __ht_is_empty(ht_t *table) 
 {
     if (table != NULL)
     {
-        return (table->arraySize) == 0; // FIXME: should this be numberOfItemsInTable ??
+        return (table->numberOfItemsInTable) == 0;
     }
     return 0;
 }
@@ -413,11 +415,11 @@ unsigned int ht_is_empty(ht_t *table)
  * @param table The hashtable to retrieve the number of elements from
  * @return The number of elements in the table (0 if table is NULL)
  */
-unsigned int ht_get_num_elements(ht_t *table)
+ht_index_t __ht_get_num_elements(ht_t *table)
 {
     if (table != NULL)
     {
-        return (table->numberOfItemsInTable);
+        return table->numberOfItemsInTable;
     }
     return 0;
 }
@@ -429,7 +431,7 @@ unsigned int ht_get_num_elements(ht_t *table)
  * @param string The string to be hashed
  * @return The hashvalue of the string, 0 if string is NULL
  */
-unsigned long ht_hash_string(const char *string)
+ht_key_t __ht_hash_string(const char *string)
 {
     if (string != NULL)
     {
@@ -437,7 +439,7 @@ unsigned long ht_hash_string(const char *string)
         // http://www.cse.yorku.ca/~oz/hash.html
         // Apparently has "better distribution of the keys"
         int c;
-        unsigned long hash = 0;
+        ht_key_t hash = 0;
         while ((c = *string++))
         {
             hash += c + (hash << 6) + (hash << 16) - hash;
@@ -456,7 +458,7 @@ unsigned long ht_hash_string(const char *string)
  * @return The hash of the key 
  *         0 if the input table is NULL
  */
-static unsigned int _ht_compute_index(ht_t *table, unsigned long key)
+static ht_index_t _ht_compute_index(ht_t *table, ht_key_t key)
 {
     if (table != NULL)
     {
@@ -510,7 +512,7 @@ static void _ht_double_table(ht_t *table)
     if (table != NULL)
     {
         // Keep a reference to the old table in the iterator
-        ht_itr_t *itr = ht_create_iterator(table);
+        ht_itr_t *itr = __ht_create_iterator(table);
         table->arraySize = table->arraySize * 2;
         table->table = malloc(sizeof(ht_entry_t *) * table->arraySize);
 
@@ -519,13 +521,13 @@ static void _ht_double_table(ht_t *table)
         table->numberOfItemsInTable = 0;
         table->numberOfSlotsUsed = 0;
 
-        for (unsigned int i = 0; i < table->arraySize; i++)
+        for (ht_index_t i = 0; i < table->arraySize; i++)
         {
             table->table[i] = NULL;
         }
 
         // Loop throught the original table and rehash them into the new table
-        while (ht_iterator_has_next(itr))
+        while (__ht_iterator_has_next(itr))
         {
             ht_entry_t *node = __ht_iterator_next(itr);
             __ht_put(table, node->key, node->value);
@@ -535,7 +537,7 @@ static void _ht_double_table(ht_t *table)
         // Free the old table
         free(itr->iteratorTable);
 
-        ht_iterator_free(&itr);
+        __ht_iterator_free(&itr);
     }
 }
 
@@ -550,7 +552,7 @@ static void _ht_half_table(ht_t *table)
     if (table != NULL && (table->arraySize) >= (2 * HT_INITIAL_SIZE))
     {
         // Keep a reference to the old table in the iterator
-        ht_itr_t *itr = ht_create_iterator(table);
+        ht_itr_t *itr = __ht_create_iterator(table);
 
         table->arraySize = (table->arraySize) / 2;
         table->table = malloc(sizeof(ht_entry_t *) * table->arraySize);
@@ -560,13 +562,13 @@ static void _ht_half_table(ht_t *table)
         table->numberOfItemsInTable = 0;
         table->numberOfSlotsUsed = 0;
 
-        for (unsigned int i = 0; i < table->arraySize; i++)
+        for (ht_index_t i = 0; i < table->arraySize; i++)
         {
             table->table[i] = NULL;
         }
 
         // Loop through all the values in the original table, rehashing them into the new one
-        while (ht_iterator_has_next(itr))
+        while (__ht_iterator_has_next(itr))
         {
             // TODO: Refactor this so that a the old nodes
             // are not reallocated everytime the HT changes
@@ -580,7 +582,7 @@ static void _ht_half_table(ht_t *table)
         // Free the old table
         free(itr->iteratorTable);
         
-        ht_iterator_free(&itr);
+        __ht_iterator_free(&itr);
     }
 }
 
@@ -592,7 +594,7 @@ static void _ht_half_table(ht_t *table)
  * @return A reference to the new HashTableIterator
  *         NULL if the input table is NULL
  */
-ht_itr_t *ht_create_iterator(ht_t * table)
+ht_itr_t *__ht_create_iterator(ht_t * table)
 {
     if (table != NULL)
     {
@@ -614,10 +616,10 @@ ht_itr_t *ht_create_iterator(ht_t * table)
  * Check if there are any more remaining elements in the ht_itr_t
  * 
  * @param itr The iterator to use for checking
- * @return 1 if there are more elements,
- *         0 if there are no remaining elements (or input table is null)
+ * @return true if there are more elements,
+ *         false if there are no remaining elements (or input table is null)
  */
-int ht_iterator_has_next(ht_itr_t * itr)
+bool __ht_iterator_has_next(ht_itr_t * itr)
 {
     if (itr != NULL)
     {
@@ -640,7 +642,7 @@ ht_entry_t *__ht_iterator_next(ht_itr_t * itr)
 {
     if (itr != NULL)
     {
-        if (ht_iterator_has_next(itr))
+        if (__ht_iterator_has_next(itr))
         {
             if (itr->currentNode != NULL)
             {
@@ -667,7 +669,7 @@ ht_entry_t *__ht_iterator_next(ht_itr_t * itr)
  * 
  * @param itr The iterator to free
  */
-void ht_iterator_free(ht_itr_t **itr)
+void __ht_iterator_free(ht_itr_t **itr)
 {
     if (itr != NULL && *itr != NULL)
     {
